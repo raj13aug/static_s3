d10d15a273ca8d10e2684d05e2c591ed5a40c28f
Nataraj Ramamoorthy <nataraj.ramamoorthy85@gmail.com>
2022-12-04 17:59:40 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -9,6 +9,9 @@ resource "aws_s3_bucket" "example" {
 
 resource "aws_s3_bucket_website_configuration" "example-config" {
   bucket = aws_s3_bucket.example.bucket
+  index_document = {
+    suffix = "index.html"
+  }
 }
 
 resource "aws_s3_bucket_policy" "example-policy" {
