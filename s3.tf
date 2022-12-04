1e3f5dd09d74525d03a3d34ab8162d544294a09a
Nataraj Ramamoorthy <nataraj.ramamoorthy85@gmail.com>
2022-12-04 17:49:38 +0530

    first commit

---
+++ s3.tf
@@ -0,0 +1,23 @@
+resource "aws_s3_bucket" "example" {
+  bucket = var.bucketName
+}
+
+resource "aws_s3_bucket_website_configuration" "example-config" {
+  bucket = aws_s3_bucket.example.bucket
+  index_document = {
+    suffix = "index.html"
+  }
+}
+
+resource "aws_s3_bucket_policy" "example-policy" {
+  bucket = aws_s3_bucket.example.id
+  policy = templatefile("templates/s3-policy.json", { bucket = var.bucketName })
+}
+
+
+resource "aws_s3_object" "example-index" {
+  bucket = aws_s3_bucket.example.id
+  key    = "index.html"
+  source = "../index.html"
+  acl    = "public-read"
+}
---
+++ variables.tf
@@ -0,0 +1,4 @@
+variable "bucketName" {
+  default = "cloudroot-007"
+  type    = string
+}
